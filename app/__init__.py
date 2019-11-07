71d2362bd332a0b9ffa211c87d787aac31598644
Vasily <shiawasena.hoshi@gmail.com>
2019-11-07 23:43:39 +0300

    - autocreating folders - remove compressed images still not working

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,15 @@
 import os
+import re
+from threading import Thread
 
 import telebot
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from flask import current_app
 from yadisk import yadisk
 
 from config import Config
-from threading import Thread
-import re
+
 db = SQLAlchemy()
 migrate = Migrate()
 
@@ -27,25 +27,48 @@ def create_app(config_class=Config):
 
 def start_echo_bot():
     # with app.app_context():
-        bot = telebot.TeleBot(Config.TG_TOKEN)
-        y = yadisk.YaDisk(token=Config.YD_TOKEN)
-        @bot.message_handler(content_types=["text"])
-        def repeat_all_messages(message):  # Название функции не играет никакой роли, в принципе
-            bot.send_message(message.chat.id, message.text)
+    bot = telebot.TeleBot(Config.TG_TOKEN)
+    y = yadisk.YaDisk(token=Config.YD_TOKEN)
 
-        @bot.message_handler(content_types=["document"])
-        # @bot.message_handler(func=lambda message: message.document and message.text.lower())
-        def it_is_image(message):
-            if re.search('^.+/(jpg|jpeg|avi|mov|mp4)$', message.document.mime_type).group(0):
-                file_info = bot.get_file(message.document.file_id)
-                downloaded_file = bot.download_file(file_info.file_path)
-                local_path = Config.DOWNLOAD_FOLDER + "/" + message.document.file_id + "_" + message.document.file_name
-                yd_path = Config.YD_DOWNLOAD_FOLDER + "/" + message.document.file_id + "_" + message.document.file_name
-                with open(local_path, 'w+b') as new_file:
-                    new_file.write(downloaded_file)
-                if not y.exists(yd_path):
-                    with open(local_path, "rb") as f:
-                        y.upload(f, yd_path)
-                os.remove(local_path)
+    def get_chat_folder(chat_name):
+        path = Config.YD_DOWNLOAD_FOLDER + "/" + chat_name
+        if not y.exists(path):
+            y.mkdir(path)
+        return path
 
-        bot.polling(none_stop=True)
+    get_chat_folder("")
+
+    # @bot.message_handler(content_types=["text"])
+    # def repeat_all_messages(message):
+    #     bot.send_message(message.chat.id, message.text)
+
+    # @bot.message_handler(content_types=["photo"],
+    #                      func=lambda message: message.chat.title is not None and message.from_user == Config.TG_ADMIN_ID)
+    # def delete_image(message):
+    #     bot.send_message(message.chat.id, "Удалить спам!")
+
+    @bot.message_handler(func=lambda message: message.chat.title is not None and is_extension_ok(message), content_types=['document'])
+    def save_file(message):
+        file_info = bot.get_file(message.document.file_id)
+        chat_name = message.chat.title
+        if chat_name:
+            downloaded_file = bot.download_file(file_info.file_path)
+            local_path = Config.DOWNLOAD_FOLDER + "/" + message.document.file_id + "_" + message.document.file_name
+            yd_path = get_chat_folder(chat_name) + "/" + message.document.file_id + "_" + message.document.file_name
+            with open(local_path, 'w+b') as new_file:
+                new_file.write(downloaded_file)
+            if not y.exists(yd_path):
+                with open(local_path, "rb") as f:
+                    y.upload(f, yd_path)
+            os.remove(local_path)
+
+    @bot.message_handler(func=lambda message: message.chat.title is None and is_extension_ok(message), content_types=['document'])
+    def delete_file(message):
+        pass
+
+
+
+    def is_extension_ok(message):
+        return message.content_type == "document" and re.search('^.+/(jpg|jpeg|avi|mov|mp4)$', message.document.mime_type).group(0) is not None
+
+    bot.polling(none_stop=True)
