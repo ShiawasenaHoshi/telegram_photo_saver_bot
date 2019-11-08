3ab7c751014542c9c4f954734c76074933cc2586
Vasily <shiawasena.hoshi@gmail.com>
2019-11-08 11:06:44 +0300

    - file_name from exif

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,14 @@ import re
 from threading import Thread
 
 import telebot
+import datetime
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from yadisk import yadisk
 
 from config import Config
+from exif import Image
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -21,22 +23,26 @@ def create_app(config_class=Config):
     db.init_app(app)
     migrate.init_app(app, db)
 
-    Thread(target=start_echo_bot).start()
+    Thread(target=start_bot).start()
     return app
 
 
-def start_echo_bot():
+def start_bot():
     # with app.app_context():
     bot = telebot.TeleBot(Config.TG_TOKEN)
     y = yadisk.YaDisk(token=Config.YD_TOKEN)
 
-    def get_chat_folder(chat_name):
-        path = Config.YD_DOWNLOAD_FOLDER + "/" + chat_name
+    def get_upload_folder(chat_name, folder_date=None):
+        if folder_date is None:
+            path = Config.YD_DOWNLOAD_FOLDER + "/" + chat_name
+        else:
+            name = folder_date.strftime('%Y_%m_%d')
+            path = get_upload_folder(chat_name) + "/" + name
         if not y.exists(path):
             y.mkdir(path)
         return path
 
-    get_chat_folder("")
+    get_upload_folder("")
 
     @bot.message_handler(content_types=["photo"], func=lambda
             message: message.chat.title is not None and message.from_user.id != Config.TG_ADMIN_ID)
@@ -52,9 +58,13 @@ def start_echo_bot():
         if chat_name:
             downloaded_file = bot.download_file(file_info.file_path)
             local_path = Config.DOWNLOAD_FOLDER + "/" + message.document.file_id + "_" + message.document.file_name
-            yd_path = get_chat_folder(chat_name) + "/" + message.document.file_id + "_" + message.document.file_name
             with open(local_path, 'w+b') as new_file:
                 new_file.write(downloaded_file)
+            with open(local_path, 'rb') as image_file:
+                img = Image(image_file)
+                dt_str = img.datetime
+                dt = datetime.datetime.strptime(dt_str, '%Y:%m:%d %H:%M:%S')
+                yd_path = get_upload_folder(chat_name, dt) + "/" + get_file_name(message, dt)
             if not y.exists(yd_path):
                 with open(local_path, "rb") as f:
                     y.upload(f, yd_path)
@@ -65,6 +75,8 @@ def start_echo_bot():
     def delete_file(message):
         pass
 
+    def get_file_name(message, dt):
+        return dt.strftime('%H_%M_%S') + "_" + message.document.file_name
     def is_extension_ok(message):
         return re.search('^.+/(jpg|jpeg|avi|mov|mp4)$', message.document.mime_type).group(0) is not None
 
