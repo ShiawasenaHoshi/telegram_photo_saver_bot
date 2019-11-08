fc4cf1fd218264fd921914516bfb2c46f5c8e6ed
Vasily <shiawasena.hoshi@gmail.com>
2019-11-08 18:09:48 +0300

    - writing db-classes

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,17 @@
+import datetime
 import os
 import re
 from threading import Thread
 
 import telebot
-import datetime
-from flask import Flask
+from exif import Image
+from flask import Flask, current_app
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+
 from yadisk import yadisk
 
 from config import Config
-from exif import Image
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -23,61 +24,67 @@ def create_app(config_class=Config):
     db.init_app(app)
     migrate.init_app(app, db)
 
-    Thread(target=start_bot).start()
+    Thread(target=start_bot, args=(app,)).start()
     return app
 
 
-def start_bot():
-    # with app.app_context():
-    bot = telebot.TeleBot(Config.TG_TOKEN)
-    y = yadisk.YaDisk(token=Config.YD_TOKEN)
+def start_bot(app):
+    from app.models import Chat, Photo
+    with app.app_context():
+        bot = telebot.TeleBot(Config.TG_TOKEN)
+        y = yadisk.YaDisk(token=Config.YD_TOKEN)
 
-    def get_upload_folder(chat_name, folder_date=None):
-        if folder_date is None:
-            path = Config.YD_DOWNLOAD_FOLDER + "/" + chat_name
-        else:
-            name = folder_date.strftime('%Y_%m_%d')
-            path = get_upload_folder(chat_name) + "/" + name
-        if not y.exists(path):
-            y.mkdir(path)
-        return path
+        def get_upload_folder(chat_name, folder_date=None):
+            if folder_date is None:
+                path = Config.YD_DOWNLOAD_FOLDER + "/" + chat_name
+            else:
+                name = folder_date.strftime('%Y_%m_%d')
+                path = get_upload_folder(chat_name) + "/" + name
+            if not y.exists(path):
+                y.mkdir(path)
+            return path
 
-    get_upload_folder("")
+        get_upload_folder("")
 
-    @bot.message_handler(content_types=["photo"], func=lambda
-            message: message.chat.title is not None and message.from_user.id != int(Config.TG_ADMIN_ID))
-    def delete_compressed_image(message):
-        bot.delete_message(message.chat.id, message.message_id)
-        bot.send_message(message.chat.id, "Фотографии можно отправлять только файлом")
+        @bot.message_handler(content_types=["photo"], func=lambda
+                message: message.chat.title is not None and message.from_user.id != int(Config.TG_ADMIN_ID))
+        def delete_compressed_image(message):
+            bot.delete_message(message.chat.id, message.message_id)
+            bot.send_message(message.chat.id, "Фотографии можно отправлять только файлом")
 
-    @bot.message_handler(func=lambda message: message.chat.title and is_extension_ok(message),
-                         content_types=['document'])
-    def save_file(message):
-        file_info = bot.get_file(message.document.file_id)
-        chat_name = message.chat.title
-        if chat_name:
-            downloaded_file = bot.download_file(file_info.file_path)
-            local_path = Config.DOWNLOAD_FOLDER + "/" + message.document.file_id + "_" + message.document.file_name
-            with open(local_path, 'w+b') as new_file:
-                new_file.write(downloaded_file)
-            with open(local_path, 'rb') as image_file:
-                img = Image(image_file)
-                dt_str = img.datetime
-                dt = datetime.datetime.strptime(dt_str, '%Y:%m:%d %H:%M:%S')
-                yd_path = get_upload_folder(chat_name, dt) + "/" + get_file_name(message, dt)
-            if not y.exists(yd_path):
-                with open(local_path, "rb") as f:
-                    y.upload(f, yd_path)
-            os.remove(local_path)
+        @bot.message_handler(func=lambda message: message.chat.title and is_extension_ok(message),
+                             content_types=['document'])
+        def save_file(message):
+            file_info = bot.get_file(message.document.file_id)
+            chat_name = message.chat.title
+            if chat_name:
+                downloaded_file = bot.download_file(file_info.file_path)
+                local_path = Config.DOWNLOAD_FOLDER + "/" + message.document.file_id + "_" + message.document.file_name
+                with open(local_path, 'w+b') as new_file:
+                    new_file.write(downloaded_file)
+                with open(local_path, 'rb') as image_file:
+                    img = Image(image_file)
+                    dt_str = img.datetime
+                    dt = datetime.datetime.strptime(dt_str, '%Y:%m:%d %H:%M:%S')
+                    yd_path = get_upload_folder(chat_name, dt) + "/" + get_file_name(message, dt)
 
-    @bot.message_handler(func=lambda message: message.chat.title is None and is_extension_ok(message),
-                         content_types=['document'])
-    def delete_file(message):
-        pass
+                if not y.exists(yd_path):
+                    with open(local_path, "rb") as f:
+                        with app.app_context():
+                            chat = Chat.get_chat(message)
+                            photo = Photo.get_photo(local_path, message)
+                            y.upload(f, yd_path)
+                os.remove(local_path)
 
-    def get_file_name(message, dt):
-        return dt.strftime('%H_%M_%S') + "_" + message.document.file_name
-    def is_extension_ok(message):
-        return re.search('^.+/(jpg|jpeg|avi|mov|mp4)$', message.document.mime_type).group(0) is not None
+        @bot.message_handler(func=lambda message: message.chat.title is None and is_extension_ok(message),
+                             content_types=['document'])
+        def delete_file(message):
+            pass
 
-    bot.polling(none_stop=True)
+        def get_file_name(message, dt):
+            return dt.strftime('%H_%M_%S') + "_" + message.document.file_name
+
+        def is_extension_ok(message):
+            return re.search('^.+/(jpg|jpeg|avi|mov|mp4)$', message.document.mime_type).group(0) is not None
+
+        bot.polling(none_stop=True)
--- app/models.py
+++ app/models.py
@@ -1,27 +1,30 @@
 from app import db
+from app.generic import calc_hash
 from config import Config
 
 
 class Photo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    hash = db.Column(db.String(240), nullable=False) #fixme
+    file_hash = db.Column(db.String(240), nullable=False)
     chat_id = db.Column(db.Integer, db.ForeignKey('chat.id'), nullable=False)
     user_id = db.Column(db.Integer, nullable=False)
     msg_date = db.Column(db.DateTime, nullable=False)
     filename = db.Column(db.String(240), nullable=False)
 
-    # @staticmethod
-    # def get_photo(message):
-    #     photo = Photo.query.filter_by(id=message.document.id).first()
-    #     if chat is None:
-    #         chat = Chat()
-    #         chat.id = message.chat.chat_id
-    #         chat.name = message.chat.title
-    #         chat.local_folder = Config.DOWNLOAD_FOLDER + "/" + chat.name
-    #         chat.yd_folder = Config.YD_DOWNLOAD_FOLDER + "/" + chat.name
-    #     return chat
-
-
+    @staticmethod
+    def get_photo(photo_file, message):
+        h = calc_hash(photo_file)
+        photo = Photo.query.filter_by(chat_id=message.chat.id, hash=h).first()
+        if photo is None:
+            photo = Photo()
+            photo.file_hash = h
+            photo.chat_id = message.chat.id
+            photo.user_id = message.from_user.id
+            photo.msg_date = message.date
+            photo.filename = "NULL"  # fixme
+            db.session.add(photo)
+            db.session.commit()
+        return photo
 
 
 class Chat(db.Model):
@@ -32,11 +35,13 @@ class Chat(db.Model):
 
     @staticmethod
     def get_chat(message):
-        chat = Chat.query.filter_by(id=message.chat.chat_id).first()
+        chat = Chat.query.filter_by(id=message.chat.id).first()
         if chat is None:
             chat = Chat()
-            chat.id = message.chat.chat_id
+            chat.id = message.chat.id
             chat.name = message.chat.title
             chat.local_folder = Config.DOWNLOAD_FOLDER + "/" + chat.name
             chat.yd_folder = Config.YD_DOWNLOAD_FOLDER + "/" + chat.name
+            db.session.add(chat)
+            db.session.commit()
         return chat
