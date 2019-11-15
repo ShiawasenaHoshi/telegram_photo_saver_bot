3f51a96e91d558e8a38efbb1cd15f636cd8f4bd7
Vasily <shiawasena.hoshi@gmail.com>
2019-11-15 22:33:29 +0300

    - minor improvements

--- app/__init__.py
+++ app/__init__.py
@@ -17,8 +17,10 @@ def create_app(config_class=Config):
     db.init_app(app)
     migrate.init_app(app, db)
     app.logger.setLevel(logging.INFO)
-    if not config_class.TESTING:
+    if not config_class.TESTING and config_class.TG_TOKEN and config_class.TG_ADMIN_ID and config_class.YD_TOKEN:
         from app.bot import Bot
         Bot(Config.TG_TOKEN, Config.DOWNLOAD_FOLDER, Config.YD_TOKEN, Config.YD_DOWNLOAD_FOLDER, Config.TG_ADMIN_ID,
             app).start()
+    else:
+        print("App is not configured. Check config.py")
     return app
